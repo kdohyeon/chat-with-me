6786be22f4485c1523fd5fed1d0ac56a28792ed0
kdohyeon <kdohyeon@gmail.com>
2024-12-15 14:52:16 +0900

    [NO-JIRA] 초기 설정

---
+++ cwm-backend/src/main/kotlin/kdohyeon/cwm/cwmbackend/CwmBackendApplication.kt
@@ -0,0 +1,11 @@
+package kdohyeon.cwm.cwmbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CwmBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<CwmBackendApplication>(*args)
+}
---
+++ cwm-frontend/run.sh
@@ -0,0 +1,3 @@
+docker build -t cwm-frontend .
+
+docker run -p 3000:3000 cwm-frontend
